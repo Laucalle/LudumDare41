5ead9174e4213a9a56546364a8287187a39e28c4
Javier León Palomares <jleon95@users.noreply.github.com>
2018-04-21 21:05:28 +0200

    Enemies now spawn

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody2D rb2D;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+		rb2D = GetComponent<Rigidbody2D> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector2 direction = player.transform.position - transform.position;
+		rb2D.velocity = direction * speed;
+
+		
+	}
+}
---
+++ Assets/Scripts/SpawnerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerController : MonoBehaviour {
+
+	public float spawnFreq;
+	public GameObject enemyPrefab;
+	public GameObject player;
+	private float prevSpawn;
+
+	// Use this for initialization
+	void Start () {
+
+		prevSpawn = Time.time;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Time.time - prevSpawn > spawnFreq){
+			GameObject enemy = Instantiate(enemyPrefab, transform.position, transform.rotation);
+			enemy.GetComponent<EnemyController> ().player = player;
+			prevSpawn = Time.time;
+		}
+		
+	}
+}
