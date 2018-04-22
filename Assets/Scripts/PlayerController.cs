5182e4e87927da78bb798d5e94e47b5f59db044f
José Antonio <ocetebt@hotmail.com>
2018-04-22 12:03:46 +0200

    Mergeando PlayerController y escena actualizada

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	// AÑADIDO POR JOSE
+	List<int> touching_tiles;
+	public PuzzleManager puzzle_manager;
+
     public float speed;
     public float bulletSpeed;
     public float fireRate;
@@ -61,6 +65,11 @@ public class PlayerController : MonoBehaviour {
             transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * 10);
 			//child.GetComponent<GunController>().CompensateRotation (targetRotation);
         }
+
+		// AÑADIDO POR JOSE
+		if (Input.GetKeyDown(KeyCode.L)) {
+			SwitchTiles();
+		}
     }
 
     public void FixedUpdate()
@@ -75,11 +84,69 @@ public class PlayerController : MonoBehaviour {
 		child.GetComponent<Rigidbody2D>().velocity = movement.normalized * speed;  
     }
 
-	public void OnTriggerEnter2D(Collider2D collider){
+	// AÑADIDO POR JOSE
+	public void OnTriggerEnter2D(Collider2D collider2D){
+		if (collider2D.gameObject.tag == "Tile") {
+			TileManager tile_manager = collider2D.gameObject.GetComponent<TileManager> ();
+			if (!touching_tiles.Contains (tile_manager.GetId ())) {
+				touching_tiles.Add (tile_manager.GetId ());
+			}
 
-		if(collider.transform.tag == "Enemy"){
-			Debug.Log ("Te han pillao");
-			Destroy (collider.transform.gameObject);
+			if (tile_manager.GetEndTile ()) {
+				Debug.Log ("He Ganado!");
+			} else if (tile_manager.GetDeadly ()) {
+				Die ();
+			}
+		} else if (collider2D.transform.tag == "Enemy") {
+			Debug.Log ("Te han pillado");
+			Destroy (collider2D.transform.gameObject);
 		}
+	}
+
+	// AÑADIDO POR JOSE
+	public void OnTriggerExit2D(Collider2D collider2D){
+		if (collider2D.gameObject.tag == "Tile") {
+			TileManager tile_manager = collider2D.gameObject.GetComponent<TileManager> ();
+			touching_tiles.Remove (tile_manager.GetId ());
+		}
+
+	}
+
+	// AÑADIDO POR JOSE
+	private void Die() {
+		Debug.Log ("He muerto");
+	}
+
+	// AÑADIDO POR JOSE
+	private void SwitchTiles() {
+		if (touching_tiles.Count == 1) {
+			int tile_id = touching_tiles [0];
+			int movement = puzzle_manager.TrySwitchTile (tile_id);
+			float offset = 2.57f;
+			Vector3 new_position = transform.position;
+			switch(movement) {
+			case 1:
+				new_position.x -= offset;
+				break;
+			case 2:
+				new_position.y += offset;
+				break;
+			case 3:
+				new_position.x += offset;
+				break;
+			case 4:
+				new_position.y -= offset;
+				break;
+			}
+			transform.position = new_position;
+		}
+	}
+
+	void PrintTouchingTiles() {
+		string str = "Lista: ";
+		for (int i=0; i<touching_tiles.Count; i++) {
+			str += touching_tiles[i] + " ";
+		}
+		Debug.Log (str);
 	}
 }
